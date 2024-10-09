5b39940c9ae7a2cea0de654eed2e15637c363af9
Blake Han <88338598+BlakeHan01@users.noreply.github.com>
2024-10-09 10:28:24 -0400

    Added auth code generation and storing auth code to session/

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,15 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * <p>
@@ -17,4 +22,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        // check phone number
+        // If don't match, return ERROR
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("Phone format wrong!");
+        }
+        // If does match, generate auth code
+        String code = RandomUtil.randomNumbers(6);
+
+        // 4. Save to session
+        session.setAttribute("code", code);
+
+        // Send code
+        log.debug("Send auth code successfully, code is: " + code);
+        // return OK
+        return Result.ok();
+    }
 }
